8b5f4515e549f8e09ace89de3f41b811ebdbd054
wesbdss <wesleybenicio4@gmail.com>
2020-02-14 03:44:17 -0300

    Layout Pronto

--- js/index1.js
+++ js/index1.js
@@ -1,39 +1,47 @@
 var c = document.getElementById('myCanvas');
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / 300, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer({ canvas: c });
-renderer.setSize(window.innerWidth, 300)
-
-camera.position.set(0, 0, 5);
+var camera = new THREE.PerspectiveCamera(75, 350 / 250, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer({ canvas: c, antialias: true });
+renderer.setSize(350, 250)
+camera.position.set(0, 0, 2);
 camera.lookAt(0, 0, 0);
 
+
 /*
- * Adiciona um cubo
+ * Adiciona um cubo2
  */
-var geo = new THREE.BoxGeometry();
-var mat = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var geo = new THREE.BoxGeometry(0);
+var mat = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 var cube = new THREE.Mesh(geo, mat);
-
-// scene.add(cube);
-// cube.position.set(0, 0, 0);
-
-var loader = new THREE.FontLoader();
-loader.load('./font/Monster Friend Back_Regular.js', function (font) {
-    var parametros = {
-        font: font,size: 80, height: 5, curveSegments: 12, bevelEnable: true, benelThickness: 10, bevelSize: 8, bevelOffset: 0, bevelSegments: 5
-    };
-    var text = new THREE.TextGeometry("Oie", parametros);
-    scene.add(text);
-
-});
+var mat = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube1 = new THREE.Mesh(geo, mat);
+var mat = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+var cube2 = new THREE.Mesh(geo, mat);
+cube.position.set(0,0,0);
+scene.add(cube);
+scene.add(cube1);
+scene.add(cube2);
 
 
+
+/*
+ * Adiciona um luz pontual
+ */
+
+var light = new THREE.PointLight(0xff0000,1,100);
+light.position.set(0,0,5);
+
+scene.add(light);
 
 function animate() {
     requestAnimationFrame(animate);
-
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+    cube1.rotation.x += 0.1;
+    cube2.rotation.y += 0.1;
     renderer.render(scene, camera);
 }
 
 animate();
+console.log("è pra rodar");
