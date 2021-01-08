cb5003cf98ecc33b2573d595f6ebb052a37e86e6
wesbdss <wesleybenicio4@gmail.com>
2021-01-08 17:09:42 -0300

    Updates

---
+++ precache-manifest.18f9c7e704b8d0a7cb3227d5196efa23.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907417775558d449b60e20130c428490",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fd47c5ac085388115b0",
+    "url": "/static/css/main.4c46b791.chunk.css"
+  },
+  {
+    "revision": "35a85e9355d265f0411e",
+    "url": "/static/js/2.6426d951.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/2.6426d951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd47c5ac085388115b0",
+    "url": "/static/js/main.5adbea9c.chunk.js"
+  },
+  {
+    "revision": "d24b52cb4abe27020494",
+    "url": "/static/js/runtime-main.87409604.js"
+  },
+  {
+    "revision": "d8d7cc10734de674f6f1e8fec637a546",
+    "url": "/static/media/facebook-svgrepo-com.d8d7cc10.svg"
+  },
+  {
+    "revision": "2732a71673b50f405b7f4c28ddaac291",
+    "url": "/static/media/smile-svgrepo-com.2732a716.svg"
+  },
+  {
+    "revision": "d694f24d3fd5f8df8996f091aa1b909c",
+    "url": "/static/media/video-svgrepo-com.d694f24d.svg"
+  },
+  {
+    "revision": "34ea85af14eeb2d00b6fee7569d2b5c6",
+    "url": "/static/media/video.34ea85af.svg"
+  }
+]);
